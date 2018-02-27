179b7c98c40f07256b913b69a7cab57380d767fb
marcialwushu <marcialwushu@gmail.com>
2018-02-27 10:57:42 -0300

    signin function in auth.ts service provider added

--- src/providers/auth/auth.ts
+++ src/providers/auth/auth.ts
@@ -1,17 +1,62 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Observable } from 'rxjs/Observable';
 
-/*
-  Generated class for the AuthProvider provider.
+import * as firebase from 'firebase/app';
+import { Platform } from 'ionic-angular/platform/platform';
+import { LoadingController } from 'ionic-angular/components/loading/loading-controller';
+import { DataProvider } from '../data/data';
+import { FunctionsProvider } from '../functions/functions';
 
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
+
 @Injectable()
 export class AuthProvider {
+  public user: Observable<firebase.User>;
 
-  constructor(public http: HttpClient) {
-    console.log('Hello AuthProvider Provider');
+  constructor(
+    public http: HttpClient,
+    private platform: Platform,
+    private loadingCtrl: LoadingController,
+    public afAuth: AngularFireAuth,
+    private data: DataProvider,
+    private functions: FunctionsProvider
+
+  ) {
+    this.user = afAuth.authState;
+    
+  }
+
+  createUser(email: string, password: string): Promise<any>{
+    return new Promise((resolve, reject) => {
+      firebase.auth().createUserWithEmailAndPassword(email, password).then((data) => {
+        this.functions.post('users',{}).subscribe(() => {
+          resolve();
+        });
+      }, (error) => {
+        reject(error);
+      });
+    });
+  }
+
+  signInWithEmailAndPassword(email: string, password: string): Promise<any> {
+    return new Promise((resolve, reject) => {
+      firebase.auth().signInWithEmailAndPassword(email, password).then((data) => {
+        resolve(data);
+      }, (error) =>{
+        reject(error);
+      });
+    });
+  }
+
+  logout(): Promise<any> {
+    return new Promise((resolve, reject) => {
+      firebase.auth().signOut().then(() => {
+        resolve();
+      }, (error) => {
+        reject(error);
+      });
+    });
   }
 
 }
